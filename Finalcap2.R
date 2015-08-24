355480358a439849e4a212200f95c815607695bf
mintmintt <jmint12345@gmail.com>
2015-08-24 23:30:24 +0800

    Update Finalcap2.R

--- Finalcap2.R
+++ Finalcap2.R
@@ -1,36 +1,10 @@
+# Load libraries and perform other overheads.
+library("stylo")
+library("tm")
+library("data.table")
+library("ggplot2")
+library("dplyr")
+library("shiny")
 
-library(tm)
-library(RSQLite)
-source('Finalcap.R')
-
-db <- dbConnect(SQLite(), dbname="shiny-app/train.db")
-dbSendQuery(conn=db,
-            "CREATE TABLE NGram
-            (pre TEXT,
-            word TEXT,
-            freq INTEGER,
-            n INTEGER)")  # ROWID automatically created with SQLite dialect
-
-# Get word frequencies
-freq_5 <- tdmToFreq(tdm_5)
-freq_4 <- tdmToFreq(tdm_4)
-freq_3 <- tdmToFreq(tdm_3)
-freq_2 <- tdmToFreq(tdm_2)
-
-# Process with pre and current word
-processGram(freq_5)
-processGram(freq_4)
-processGram(freq_3)
-processGram(freq_2)
-
-# Insert into SQLite database
-sql_5 <- "INSERT INTO NGram VALUES ($pre, $cur, $freq, 5)"
-bulk_insert(sql_5, freq_5)
-sql_4 <- "INSERT INTO NGram VALUES ($pre, $cur, $freq, 4)"
-bulk_insert(sql_4, freq_4)
-sql_3 <- "INSERT INTO NGram VALUES ($pre, $cur, $freq, 3)"
-bulk_insert(sql_3, freq_3)
-sql_2 <- "INSERT INTO NGram VALUES ($pre, $cur, $freq, 2)"
-bulk_insert(sql_2, freq_2)
-
-dbDisconnect(db)
+source("functions.R")  # Utility functions here.
+load("vars_final.RData")  # Loads the look-up tables, "dt_cft*".
