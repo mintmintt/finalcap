04713a1f8d1e42fc1f8a509bb02f8a3e791ab692
mintmintt <jmint12345@gmail.com>
2015-08-25 02:21:25 +0800

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,32 +1,30 @@
-
 library(shiny)
 
-## Define UI for application that draws a histogram
+# Define UI for application that draws a histogram
 shinyUI(fluidPage(
         
-        ## Application title
-        titlePanel("Predictive Word Algorithm"),
-        h4("Final Project", style="color:green"),
-        tags$head(tags$style("#text1, #text2{color: green;
-                                 font-size: 20px;
-                             }"
-  )),
-  br(),
-  br(),
-        
-        fluidRow(width=5,
-                 p("Type a sequence of words, up to 3 words. You can then select the 'NEXT' button for the prediction"),
-                 p("Using backoff model",
-                   a(href="http://www.w3.org/TR/ngram-spec/", 
-                     "Backoff"), "for unknown ngrams.")),
+        # Application title
+        titlePanel("N-Gram Word Predictor"),
+        h4("James Wang for the John Hopkins Coursera Data Science Capstone", style="color:gray"),
         hr(),
         
-        ## Sidebar with a slider input for the number of bins
+        fluidRow(width=2,
+                 p("Enter a sentence, hit enter (or press the 'Predict next' button), and see what the 
+                   algorithm thinks will come next!"),
+                 p("The underlying model is an",
+                   a(href="http://en.wikipedia.org/wiki/N-gram", "n-gram"),
+                   "model, with quadgrams from a dataset of Twitter, news articles, 
+                   and blog posts. It uses 'Stupid Backoff'",
+                   a(href="http://www.cs.columbia.edu/~smaskey/CS6998-0412/supportmaterial/langmodel_mapreduce.pdf", 
+                     "(Brants et al 2007)"), "to deal with unseen n-grams.")),
+        hr(),
+        
+        # Sidebar with a slider input for the number of bins
         sidebarLayout(
                 sidebarPanel(
-                        textInput("text", label = h3("Input"), value = "north south east"),
-                        helpText("Enter sequence of words, select the 'NEXT' button, a predicted word will appear."),
-                        submitButton("NEXT"),
+                        textInput("text", label = h3("Input"), value = "happy birthday to"),
+                        helpText("Type in a sentence above, hit enter (or press the button below), and the results will display to the right."),
+                        submitButton("Predict next"),
                         hr()
                 ),
                 
@@ -34,8 +32,10 @@ shinyUI(fluidPage(
                 mainPanel(
                         br(),
                         h2(textOutput("sentence"), align="center"),
-                        h1(textOutput("predicted"), align="center", style="color:green"),
-                        hr()
+                        h1(textOutput("predicted"), align="center", style="color:blue"),
+                        hr(),
+                        h3("Top 5 Possibilities:", align="center"),
+                        div(tableOutput("alts"), align="center")
                 )
         )
         ))
